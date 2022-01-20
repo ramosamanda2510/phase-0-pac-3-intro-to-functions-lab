9982ca84f838a7d3fe407703b15c38d5b14ed0d1
Amanda Ramos <ramosamanda2510@gmail.com>
2022-01-19 21:42:12 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+    return string.toUpperCase();
+  }
+
+  function whisper(string){
+    return string.toLowerCase();
+  }
+
+  function logShout(string){
+    return console.log('HELLO');
+  }
+
+  function logWhisper(string){
+    return console.log('hello');
+  }
+
+function sayHiToHeadphonedRoommate(string){
+
+    if (string == "hello")
+    return "I can't hear you!";
+
+    else if (string =="HELLO")
+    return "YES INDEED!";
+
+    else if (string =="Let's have dinner together!")
+    return "I would love to!";
+}
